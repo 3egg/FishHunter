793d7f86f43ccf66c7de5529ecb66469b27d0835
3egg <627096931@qq.com>
2019-06-16 18:12:02 +0800

    first commit

---
+++ Assets/Scripts/Effect/Ef_WaterWave.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ef_WaterWave : MonoBehaviour
+{
+    public Texture[] textures;
+    public Material material;
+    private int index = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        material = GetComponent<MeshRenderer>().material;
+        InvokeRepeating("ChangeTexture", 0f, 0.1f);
+    }
+
+    void ChangeTexture()
+    {
+        material.mainTexture = textures[index];
+        index = (index + 1) % textures.Length; //0+1 / 5 = 4 ,  1+1 / 5 = 3, 3+1/5 = 2, 4+1/5=1
+    }
+}
---
+++ Assets/Scripts/GunFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunFollow : MonoBehaviour
+{
+    public RectTransform UGUICavas;
+    public Camera mainCamera;
+
+    void Update()
+    {
+        Vector2 vector2 = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector3 mousePos;
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(UGUICavas, vector2, mainCamera, out mousePos);
+        float z;
+        if (mousePos.x > transform.position.x)
+        {
+            z = -Vector3.Angle(Vector3.up, mousePos - transform.position);
+        }
+        else
+        {
+            z = Vector3.Angle(Vector3.up, mousePos - transform.position);
+        }
+        transform.localRotation = Quaternion.Euler(0,0,z);
+    }
+}
